4d532bec27e9bc45c4b2eb9d11e28bce9a05227a
Matt <jones@railroad19.com>
2019-08-17 14:30:04 -0400

    add input logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="login-form">
+  <form class="login-form">
     <label for="email">
       Email:
-      <input type="text" name="email" />
+      <input 
+        type="text"
+        :name="formFields.email"
+        @input="handleInputs.call(this, $event, formFields.email)" />
     </label>
     <br />
     <label for="password">
       Password:
-      <input type="password" name="email" />
+      <input
+        type="password"
+        :name="formFields.password"
+        @input="handleInputs.call(this, $event, formFields.password)" />
     </label>
-  </div>
+    <p>
+      <button type="submit">Login</button>
+    </p>
+  </form>
 </template>
 
 <script>
 
 export default {
-  name: 'LoginForm'
+  name: 'LoginForm',
+  data () {
+    return {
+      formFields: {
+        email: 'email',
+        password: 'password'
+      },
+      payload: {}
+    }
+  },
+  methods: {
+    handleInputs (event, inputName) {
+      this.payload[inputName] = event.target.value;
+    }
+  }
 }
 
 </script>

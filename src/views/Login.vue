6f365973d1a840afc65522322bbdecfcac2606dd
Matt <jones@railroad19.com>
2019-08-16 22:36:53 -0400

    add login form component with route

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-form">
+    <label for="email">
+      Email:
+      <input type="text" name="email" />
+    </label>
+    <br />
+    <label for="password">
+      Password:
+      <input type="password" name="email" />
+    </label>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LoginForm'
+}
+
+</script>
+
+<style scoped>
+.login-form {
+  padding: 45px;
+}
+</style>
